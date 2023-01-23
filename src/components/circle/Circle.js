40d04625cefd738e4a9029915b6e37691ea39231
fhgaha <antsy1992@gmail.com>
2023-01-23 06:12:11 +0300

    line component

--- src/components/circle/Circle.js
+++ src/components/circle/Circle.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useLayoutEffect, useState } from 'react'
+import './circle.css'
 
 const Circle = (props) => {
 	const [position, setPosition] = useState({ x: 0, y: 0 })
@@ -10,15 +11,8 @@ const Circle = (props) => {
 		};
 	});
 
-	const styles = {
-		left: position.x,
-		top: position.y,
-		transitionTimingFunction: "cubic-bezier(0.18, 0.89, 0.32, 1.28)",
-		transitionDuration: "0.8s"
-	}
-
 	return (
-		<div id='circle' className="glowy" {...props} style={styles}></div>
+		<div id='circle' className="glowy" {...props} style={{left: position.x, top: position.y}}></div >
 	)
 }
 
--- src/components/sine/Sine.js
+++ src/components/sine/Sine.js
@@ -3,55 +3,71 @@ import React, { useEffect } from 'react'
 // x = Asin(a * fi + theta)
 // y = Bsin(b * theta)
 
-function calcY(x, h, f, w) {
-  // h is the amplitude of the wave
-  // x is the current x value we get every time interval
-  // 2 * PI is the length of one cycle (full circumference)
-  // f/w is the frequency fraction
-  return h - h * Math.sin(x * 2 * Math.PI * (f / w));
+var a = 19;
+var b = 18;
+
+function calcY(t, h, f, w) {
+	// h is the amplitude of the wave
+	// x is the current x value we get every time interval
+	// 2 * PI is the length of one cycle (full circumference)
+	// f/w is the frequency fraction
+	return h * (1 - Math.sin(b * t));
+	// const result = h - h * Math.sin(t * 2 * Math.PI * (f / w));
 }
 
-function drawSine(x, h, f, w, ctx) {
-  if (ctx == null) return;
-
-  ctx.clearRect(0, 0, w, h * 2);
-
-  ctx.beginPath(); // Draw a new path
-  ctx.strokeStyle = "white"; // Pick a color
-  for (var i = 0; i < x; i++) { // Loop from left side to current x
-    var y = calcY(i, h, f, w); // Calculate y value from x
-    ctx.lineTo(i, y); // Where to draw to
-  }
-  ctx.stroke(); // Draw
+function calcX(t, h, f, w) {
+	let theta = 1;
+	return w / 2 * (1 - Math.sin(a * t + (f / w)));
 }
 
-const MySine = () => {
+function drawSine(t, h, f, w, ctx) {
+	if (ctx == null) return;
+
+	ctx.clearRect(0, 0, w, h * 2);
+
+	ctx.beginPath(); // Draw a new path
+	ctx.strokeStyle = "white"; // Pick a color
+	for (var i = 0; i < t; i += 0.01) { // Loop from left side to current x
+		var x = calcX(i, h, f, w);
+		var y = calcY(i, h, f, w); // Calculate y value from x
+		ctx.lineTo(x, y); // Where to draw to
+	}
+
+	ctx.stroke(); // Draw
+}
+
+const Sine = (props) => {
 	useEffect(() => {
-		var x = 0;
+		var t = 0;
 		var c = document.getElementById("canvas"); // Grab canvas object
 		var ctx = c.getContext("2d"); // Define canvas context
 		var w = c.width; // Canvas width => Frequency is relative to this
 		var h = c.height / 2; // Canvas height over two => Amplitude: Volume
 		var f = 1; // How many cycles per canvas width => Frequency: Tone & Speed
 
-		setInterval(() => {
-			drawSine(x, h, f, w, ctx); // Call draww function every cycle
-			x++; // Increment x by 1
-			if (x > w) {
-				x = 0; // x cannot be more than canvas with, so back to 0
-				// f++; // increment frequency for demonstration
-			}
-		}, 1);
+		// setInterval(() => {
+		// 	drawSine(t, h, f, w, ctx); // Call draww function every cycle
+		// 	t++; // Increment x by 1
+		// 	if (t > w) {
+		// 		t = 0; // x cannot be more than canvas with, so back to 0
+		// 		f++; // increment frequency for demonstration
+		// 	}
+		// }, 10);
+		for (let i = 0; i < 2; i++) {
+			drawSine(8, h, f, w, ctx);
+			f += 0.1;
+		}
 	}, []);
 
 	return (
 		<canvas
 			id="canvas"
-			width="320"
-			height="120"
+			width="640"
+			height="240"
 			style={{ border: '1px solid #999' }}
+			{...props}
 		></canvas>
 	)
 }
 
-export default MySine
+export default Sine
