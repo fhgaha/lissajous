e987c73a7cac06f8dd64aa743d8c4e314c7e84fe
fhgaha <antsy1992@gmail.com>
2023-01-24 04:55:27 +0300

    path padding

--- src/components/path/Path.js
+++ src/components/path/Path.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
+const padding = 20
+
 const Path = () => {
 	const [width, setWidth] = useState(800)
 	const [height, setHeight] = useState(500)
@@ -8,15 +10,18 @@ const Path = () => {
 	useEffect(() => {
 		var f = 1; // How many cycles per canvas width => Frequency: Tone & Speed
 
-		let result = getDCode(width, height/2);
+		let result = getDCode(width - padding, height / 2 - padding)
 		setDCode(result)
-	}, []);
+	}, [])
 
 	return (
-		<svg width={width} height={height} xmlns="http://www.w3.org/2000/svg" style={{ border: '1px solid white' }}>
+		<svg width={width} height={height} xmlns="http://www.w3.org/2000/svg"
+			// style={{ border: '1px solid white' }}
+			viewBox={`${-padding} ${-padding} ${width + padding} ${height + padding}`}
+		// style={{padding: '20px'}}
+		>
 			<path fill='none' stroke="white" width='1'
 				style={{ filter: "drop-shadow(0px 0px 5px white)" }}
-				// d="M 10 10 L 300 200 L 500 300 Z"
 				d={dCode}
 			/>
 		</svg>
@@ -26,7 +31,7 @@ const Path = () => {
 export default Path
 
 var a = 19;
-var b = 18;
+var b = 7;
 
 function getDCode(width, height) {
 	let result = '';
@@ -35,7 +40,7 @@ function getDCode(width, height) {
 		let y = calcY(i, height)
 		result += result == '' ? ` M ${x} ${y}` : ` L ${x} ${y}`
 	}
-	console.log(result)
+	// console.log(result)
 	return result
 }
 
