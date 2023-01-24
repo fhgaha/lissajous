299c9c8e42db94d39878460ad1ec39c6bcc6a88a
fhgaha <antsy1992@gmail.com>
2023-01-24 10:00:23 +0300

    lissajous figure moves

--- src/components/path/Path.js
+++ src/components/path/Path.js
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from 'react'
+import { useInterval } from '../../hooks/useInterval'
 
-const padding = 20
+const pad = 20
 
 const Path = () => {
 	const [width, setWidth] = useState(800)
-	const [height, setHeight] = useState(500)
+	const [height, setHeight] = useState(600)
 	const [dCode, setDCode] = useState('')
+	const [theta, setTheta] = useState(0)
 
-	useEffect(() => {
-		var f = 1; // How many cycles per canvas width => Frequency: Tone & Speed
-
-		let result = getDCode(width - padding, height / 2 - padding)
+	useInterval(() => {
+		setTheta(theta + 0.01);
+		let result = getDCode(width - pad, height / 2 - pad, theta)
 		setDCode(result)
-	}, [])
+		// console.log({ theta, dCode });
+	}, 100);
 
 	return (
 		<svg width={width} height={height} xmlns="http://www.w3.org/2000/svg"
 			// style={{ border: '1px solid white' }}
-			viewBox={`${-padding} ${-padding} ${width + padding} ${height + padding}`}
-		// style={{padding: '20px'}}
+			viewBox={`${-pad} ${-pad} ${width + pad} ${height + pad}`}
 		>
 			<path fill='none' stroke="white" width='1'
 				style={{ filter: "drop-shadow(0px 0px 5px white)" }}
@@ -33,10 +34,10 @@ export default Path
 var a = 19;
 var b = 7;
 
-function getDCode(width, height) {
+function getDCode(width, height, theta) {
 	let result = '';
 	for (let i = 0; i < 10; i += 0.001) {
-		let x = calcX(i, 1, width)
+		let x = calcX(i, theta, width)
 		let y = calcY(i, height)
 		result += result == '' ? ` M ${x} ${y}` : ` L ${x} ${y}`
 	}
@@ -45,15 +46,9 @@ function getDCode(width, height) {
 }
 
 function calcY(t, h) {
-	// h is the amplitude of the wave
-	// x is the current x value we get every time interval
-	// 2 * PI is the length of one cycle (full circumference)
-	// f/w is the frequency fraction
 	return h * (1 - Math.sin(b * t));
-	// const result = h - h * Math.sin(t * 2 * Math.PI * (f / w));
 }
 
 function calcX(t, f, w) {
-	let theta = 1;
-	return w / 2 * (1 - Math.sin(a * t + (f / w)));
+	return w / 2 * (1 - Math.sin(a * t + f));
 }
