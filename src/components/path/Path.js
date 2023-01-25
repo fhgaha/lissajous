98292700e84f4c4119a7340102d8b514648830c8
fhgaha <antsy1992@gmail.com>
2023-01-25 04:52:17 +0300

    centered

--- src/components/path/Path.js
+++ src/components/path/Path.js
@@ -1,54 +1,84 @@
 import React, { useEffect, useState } from 'react'
 import { useInterval } from '../../hooks/useInterval'
+import './path.css'
 
-const pad = 20
+const pad = 150
 
 const Path = () => {
-	const [width, setWidth] = useState(800)
-	const [height, setHeight] = useState(600)
-	const [dCode, setDCode] = useState('')
-	const [theta, setTheta] = useState(0)
+	const [width, setWidth] = useState(400)
+	const [height, setHeight] = useState(400)
+	const [a, setA] = useState(11)
+	const [b, setB] = useState(10)
+	const [definition, setDefinition] = useState('')
+	const [theta, setTheta] = useState(1)
+	const [mousePos, setMousePos] = useState({});
 
 	useInterval(() => {
-		setTheta(theta + 0.01);
-		let result = getDCode(width - pad, height / 2 - pad, theta)
-		setDCode(result)
+		setTheta(theta + 0.005);
+		// setA(a + 0.01)
+		// console.log(a)
+		// setB(b + 0.01)
+		// console.log(b)
+
+		let def = getDCode(width/2, width/4, a, b, theta)
+		setDefinition(def)
 		// console.log({ theta, dCode });
-	}, 100);
+	}, 24);
+
+	useEffect(() => {
+		const handleMouseMove = (event) => {
+			setMousePos({ x: event.clientX, y: event.clientY });
+		};
+		window.addEventListener('mousemove', handleMouseMove);
+		return () => {
+			window.removeEventListener('mousemove', handleMouseMove);
+		};
+	}, []);
+
+	useEffect(() => {
+		// setTheta(mousePos.x / window.innerWidth * Math.PI)
+
+	}, [mousePos])
+
 
 	return (
-		<svg width={width} height={height} xmlns="http://www.w3.org/2000/svg"
-			// style={{ border: '1px solid white' }}
-			viewBox={`${-pad} ${-pad} ${width + pad} ${height + pad}`}
-		>
-			<path fill='none' stroke="white" width='1'
-				style={{ filter: "drop-shadow(0px 0px 5px white)" }}
-				d={dCode}
-			/>
-		</svg>
+		<>
+			<svg
+				// width={width} height={height}
+				xmlns="http://www.w3.org/2000/svg"
+				style={{ border: '1px solid blue' }}
+				viewBox={`${0} ${0} ${width} ${height}`}
+			>
+				<path fill='none' stroke="white" width='1'
+					transform={`translate(${width/4}, ${height/4})`}
+					style={{ filter: "drop-shadow(0px 0px 5px white)" }}
+					d={definition}
+				/>
+			</svg>
+			<b>
+				({mousePos.x}, {mousePos.y})
+			</b>
+		</>
 	)
 }
 
 export default Path
 
-var a = 19;
-var b = 7;
-
-function getDCode(width, height, theta) {
+function getDCode(width, height, a, b, theta) {
 	let result = '';
 	for (let i = 0; i < 10; i += 0.001) {
-		let x = calcX(i, theta, width)
-		let y = calcY(i, height)
+		let x = calcX(i, theta, width, a)
+		let y = calcY(i, height, b)
 		result += result == '' ? ` M ${x} ${y}` : ` L ${x} ${y}`
 	}
 	// console.log(result)
 	return result
 }
 
-function calcY(t, h) {
+function calcY(t, h, b) {
 	return h * (1 - Math.sin(b * t));
 }
 
-function calcX(t, f, w) {
+function calcX(t, f, w, a) {
 	return w / 2 * (1 - Math.sin(a * t + f));
 }
