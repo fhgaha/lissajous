a523a23148b4a44257c6f0b84f627d5b0c386d2a
fhgaha <antsy1992@gmail.com>
2023-01-30 19:19:26 +0300

    target active only after all animations

--- src/App.js
+++ src/App.js
@@ -10,44 +10,47 @@ const centerTargetSize = 50
 
 function App() {
   const [pathIsActive, setPathIsActive] = useState(false)
-  const [tagetIsActive, setTagetIsActive] = useState(true)
+  const [targetIsActive, setTargetIsActive] = useState(false)
 
   const app = useRef()
   const tl = useRef()
 
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
-      tl.current = gsap
-      .timeline()
-      .to('#watch-lissajous-text', {transform: 'scale(1)', opacity: 1, duration: 3, delay: 0.8})
-      .to('#watch-lissajous-text', {opacity: 0, duration: 1})
-      .to('.target-in-center', {opacity: 1, duration: 0.6})
+      tl.current = gsap.timeline({onComplete: () => setTargetIsActive(true)})
+        .to('#watch-lissajous-text',
+          { scale: 1, opacity: 1, duration: 3, ease: 'cubic-bezier(0.5, 1, 0.89, 1)', delay: 0.8 })
+        .to('#watch-lissajous-text', { opacity: 0, duration: 1 })
+        .to('.target-in-center', { opacity: 1, duration: 0.6 })
     }, app)
 
     return () => ctx.revert()
   }, [])
 
+  function onMouseEnter() {
+    setPathIsActive(true)
+    setTargetIsActive(false)
+  }
+
   return (
-    <div 
-    ref={app} 
-    className="App">
+    <div
+      ref={app}
+      className="App">
       {/* <Circle /> */}
       <WatchLissajousText />
       <Path className={pathIsActive ? 'path active' : 'path'} rotate={pathIsActive} />
 
       <div
         // className="target-in-center"
-        className={tagetIsActive ? "target-in-center" : "target-in-center inactive"}
+        className={targetIsActive ? "target-in-center" : "target-in-center inactive"}
         style={{
           width: `${centerTargetSize}px`,
           height: `${centerTargetSize}px`,
           left: `calc(50% - ${centerTargetSize}px/2)`,
           top: `calc(50% - ${centerTargetSize}px/2)`,
+          pointerEvents: `${targetIsActive ? 'all' : 'none'}`
         }}
-        onMouseEnter={() => {
-          setPathIsActive(true)
-          setTagetIsActive(false)
-        }}
+        onMouseEnter={onMouseEnter}
       ></div>
     </div>
   );
