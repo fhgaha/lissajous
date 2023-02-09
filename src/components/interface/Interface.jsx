0b5d82250ef030c3d60046149d4ee7fcc14a0df3
fhgaha <antsy1992@gmail.com>
2023-02-09 19:19:03 +0300

    interface styles

--- src/components/interface/Interface.jsx
+++ src/components/interface/Interface.jsx
@@ -1,32 +1,35 @@
 import React, { useState } from 'react'
 import './interface.scss'
 
+const numberInputWidth = 40
+
 const Interface = ({ id, symbol, onChange, inputSettings }) => {
 	const [value, setValue] = useState(0)
-	const [numberPosition, setNumberPosition] = useState({value: 0, offset: 0})
+	const [numberPosition, setNumberPosition] = useState({ value: 0, offset: 0 })
 
 	function handleChange(e) {
 		let newValue = e.target.value == '' ? 0 : parseFloat(e.target.value)
 		setValue(newValue)
-		onChange(e)
 
 		let newPos = (newValue - inputSettings.min) * 100 / (inputSettings.max - inputSettings.min)
-		let newOffset = -40 * newPos /100
-		setNumberPosition({value: newPos, offset: newOffset})
+		let newOffset = -numberInputWidth * newPos / 100
+		setNumberPosition({ value: newPos, offset: newOffset })
+		
+		onChange(e)
 	}
 
 	return (
 		<div className='interface'>
-			<div>{symbol}</div>
+			<div className='symbol'>{symbol}</div>
 			<div className="range-with-number">
 				<input type='number' className='number-input' id={id}
-					value={value == NaN ? 0 : value}
+					value={value}
 					onChange={handleChange}
 					style={{ left: `calc(${numberPosition.value}% + (${numberPosition.offset}px))` }}
 					{...inputSettings}
 				></input>
 				<input type="range" className="range-style"
-					value={value == NaN ? 0 : value}
+					value={value}
 					onChange={handleChange}
 					{...inputSettings}
 				></input>
